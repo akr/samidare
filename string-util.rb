9e2f589219e6e664e00aed878f38423bc2a26334
Tanaka Akira <akr@fsij.org>
2003-08-10 23:52:25 +0000

    generate LIRS file.

--- string-util.rb
+++ string-util.rb
@@ -5,7 +5,15 @@ class String
   def decode_gzip
     Zlib::GzipReader.new(StringIO.new(self)).read
   end
-  
+
+  def encode_gzip
+    out = ""
+    Zlib::GzipWriter.wrap(StringIO.new(out)) {|gz|
+      gz << self
+    }
+    out
+  end
+
   def decode_deflate
     Zlib::Inflate.inflate(self)
   end
