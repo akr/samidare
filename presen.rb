4d458eaa6e385f27ffab269df05b0177a214544d
Tanaka Akira <akr@fsij.org>
2005-06-27 18:00:09 +0000

    refactor out current_cache.

--- presen.rb
+++ presen.rb
@@ -3,4 +3,23 @@ class Presen
     @data = data
   end
   attr_reader :data
+
+  def current_cache(entry)
+    entry['_log'].compact.reverse.inject(nil) {|h0, h1|
+      if !h1['content'] || !h1['content'].exist?
+        h0
+      elsif !h0
+        h1
+      elsif (h0['checksum'] &&
+             h1['checksum'] &&
+             h0['checksum'] == h1['checksum']) ||
+            (h0['checksum_filtered'] &&
+             h1['checksum_filtered'] &&
+             h0['checksum_filtered'] == h1['checksum_filtered'])
+        h1
+      else
+        break h0
+      end
+    }
+  end
 end
